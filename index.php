b7763bf2f25cd3bc2e43a6d31e3f03498aa51583
jerzabongco <34363144+jerzabongco@users.noreply.github.com>
2018-02-01 19:13:32 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id ="image" >
+     <img src="1.JPG" >
+      <img src="2.JPG" >
+       <img src="3.JPG" 
+        <img src="4.JPG" >
+         <img src="5.JPG" >
+          <img src="6.JPG" >
+          <img src="7.JPG">
+	</div> 
+
+</body>
+</html>
